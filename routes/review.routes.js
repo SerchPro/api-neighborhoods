d92ba7f05c7d830af52b4b025d91c6568b6624f3
SerchPro <proyects_serch@hotmail.com>
2022-03-05 15:42:38 -0600

    deleting useless code

--- routes/review.routes.js
+++ routes/review.routes.js
@@ -1,44 +1,24 @@
 /**
- * User routes 
- * host + /api/auth
+ * User routes
+ * host + /api/review
  */
 
 const router = require("express").Router();
 const { check } = require('express-validator');
 
-const isLoggedOut = require("../middleware/isLoggedOut");
-const isLoggedIn = require("../middleware/isLoggedIn");
+const {createReview, getReview, updateReview, deleteReview,} = require('../controllers/review.controller');
 
-const { login, signup, logout, recreateToken} = require('../controllers/auth.controller');
-const { emailExists,usernameExists } = require("../helpers/db-validator");
-const { validateFileds } = require('../middleware/validator_fields');
-const { validateJWT } = require("../middleware/validate-jwt");
-const { isDate } = require("../helpers/isDate");
+//const { validateJWT } = require("../middleware/validate-jwt");
 
 
-router.use( validateJWT )
+//router.use( validateJWT )
 
-router.post("/", [
-    isLoggedOut,
-    check('password', 'Please provide a  password.').not().isEmpty(),
-    check('username', 'Please provide a  username.').not().isEmpty(),
-    check('email', 'Please provide a correct email.').isEmail(),
-    check('birthday', 'Please provide a correct birthday.').custom( isDate),
-    check('email').custom(emailExists),
-    check('username').custom(usernameExists),
-    check('password').custom(correctPassword),
-    validateFileds
-    ] , signup);
+router.post("/", createReview);
 
-router.get("/", [
-    isLoggedOut,
-    check('username', 'Please provide a  username.').not().isEmpty(),
-    check('password', 'Your password needs to be at least 8 characters long').not().isEmpty(),
-    validateFileds
-    ] , login);
+router.get("/", getReview);
 
-router.put("/", [isLoggedIn], logout);
+router.put("/", updateReview);
 
-router.delete("/", recreateToken);
+router.delete("/", deleteReview);
 
 module.exports = router;
