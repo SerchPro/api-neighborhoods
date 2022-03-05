606b508638fc74db36098d7955c17c63a47b129c
SerchPro <proyects_serch@hotmail.com>
2022-03-05 15:43:58 -0600

    adding new filelds to feature and review model

--- models/Features.model.js
+++ models/Features.model.js
@@ -2,24 +2,51 @@ const { Schema, model } = require("mongoose");
 
 const featuresSchema = new Schema(
     {
-        title: {
-            type: String,
-            required: true
+        price: {
+            type: Number,
         },
-        description: {
-            type: String
+        state: {
+            type: String,
+            enum: [" ", "Nuevo", "Usado"],
+            default: " "
+        },
+        delivery_type: {
+            type: String,
+            enum: [" ","Establecimiento", "Servicio_a_domicilio", "punto_medio"],
+            default: " "
+        },
+        payment_method :{
+            type: String,
+            enum: [" ","Establecimiento", "Servicio_a_domicilio", "punto_medio"],
+            default: " "
+        },
+        property_type :{
+            type: String,
+            enum: [" ", "Casa", "Departamento", "Terreno", "Negocio", "Otro"],
+            default: " "
+        },
+        contract_type :{
+            type: String,
+            enum: [" ", "Renta", "Venta", "Traspaso", "Otro"],
+            default: " "
         },
         start: {
             type: Date,
             required: true
         },
+        marca: {
+            type: String,
+        },
+        modelo: {
+            type: Number,
+        },
         end: {
             type: Date,
             required: true
         },
-        user: {
+        _post: {
             type:Schema.Types.ObjectId,
-            ref: 'User'
+            ref: 'Post'
         }
     },
     {
--- models/Review.model.js
+++ models/Review.model.js
@@ -5,13 +5,13 @@ const reviewSchema = new Schema(
         title: {
             type: String,
             required: [true, "Title is required"],
-            min: [3, "Title must be at least 3 characters"],
+            min: [2, "Title must be at least 2 characters"],
             max: [50, "Title must be at most 50 characters"],
         },
         comment: {
             type: String,
             required: [true, "Comment is required"],
-            min: [3, "Comment must be at least 3 characters"],
+            min: [4, "Comment must be at least 4 characters"],
             max: [2000, "Comment must be at most 2000 characters"],
         },
         _user: {
