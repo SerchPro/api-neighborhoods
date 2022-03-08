51b120ad105dbfed2d8ee5cb989c32f9d7525d1c
SerchPro <proyects_serch@hotmail.com>
2022-03-07 20:56:45 -0600

    git add upload-file

---
+++ helpers/upload-file.js
@@ -0,0 +1,28 @@
+const cloudinary = require('cloudinary').v2
+cloudinary.config(process.env.CLOUDINARY_URL);
+
+async function uploadFile(image_url, archivo) {
+    try {
+        if (image_url) {
+            if (!image_url.includes('google') || !image_url.includes('notFound_wpeppw.jpg')) {
+                const nombreArr = image_url.split('/');
+                const nombre = nombreArr[nombreArr.length - 1];
+                const [public_id] = nombre.split('.');
+                cloudinary.uploader.destroy(public_id);
+            }
+        }
+
+        const { tempFilePath } = archivo;
+        const { secure_url } = await cloudinary.uploader.upload(tempFilePath);
+
+        return secure_url
+
+    } catch (e) {
+        console.log("error to upload the file ", e);
+        return false;
+    }
+}
+
+module.exports = {
+    uploadFile
+};
